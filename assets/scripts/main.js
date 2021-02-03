167db371d5ade68ce95c6a06c00e63bf58c59c28
Sash132 <39384705+Sash132@users.noreply.github.com>
2021-02-02 17:25:12 -0800

    Added audio for button clicks

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,18 +4,21 @@ let car_horn = document.getElementById("radio-car-horn");
 let party_horn = document.getElementById("radio-party-horn");
 let sound_image = document.getElementById("sound-image");
 
-air_horn.addEventListener("change", click_airhorn);
-car_horn.addEventListener("change", click_carhorn);
-party_horn.addEventListener("change", click_partyhorn);
+air_horn.addEventListener("click", click_airhorn);
+car_horn.addEventListener("click", click_carhorn);
+party_horn.addEventListener("click", click_partyhorn);
 
 function click_airhorn() {
+  audio.src = "./assets/media/audio/air-horn.mp3";
   sound_image.src = "./assets/media/images/air-horn.svg";
 }
 
 function click_carhorn() {
+  audio.src = "./assets/media/audio/car-horn.mp3";
   sound_image.src = "./assets/media/images/car.svg";
 }
 
 function click_partyhorn() {
+  audio.src = "./assets/media/audio/party-horn.mp3";
   sound_image.src = "./assets/media/images/party-horn.svg";
 }
