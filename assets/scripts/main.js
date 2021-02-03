b5034c7343802339893ac8e523f1754e174c7af8
Sash132 <39384705+Sash132@users.noreply.github.com>
2021-02-02 17:10:47 -0800

    Test horn changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,24 @@
-// main.js
+//main.js
+let air_horn = document.getElementById("radio-air-horn");
+let car_horn = document.getElementById("radio-car-horn");
+let party_horn = document.getElementById("radio-party-horn");
+let sound_image = document.getElementById("sound-image");
 
-// TODO
+air_horn.addEventListener("click", click_airhorn);
+car_horn.addEventListener("click", click_carhorn);
+party_horn.addEventListener("click", click_partyhorn);
+
+function click_airhorn() {
+  sound_image.src = "./assets/media/images/air-horn.svg";
+  audio.src = "./assets/media/audio/air-horn.mp3";
+}
+
+function click_carhorn() {
+  sound_image.src = "./assets/media/images/car.svg";
+  audio.src = "./assets/media/audio/car-horn.mp3";
+}
+
+function click_partyhorn() {
+  audio.src = "./assets/media/audio/party-horn.mp3";
+  sound_image.src = "./assets/media/images/party-horn.svg";
+}
